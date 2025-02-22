96cb41b096c3003dedf2e5cf98751474c66cbee0
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-02-22 03:38:46 -0300

    adicionando navbar

--- frontprodutos/src/App.jsx
+++ frontprodutos/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-
 import Login from "./Components/Login/Login";
-
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Login></Login>
     </div>
   );
---
+++ frontprodutos/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <a href="#">Produto</a>
+        <a href="#">Produto</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
