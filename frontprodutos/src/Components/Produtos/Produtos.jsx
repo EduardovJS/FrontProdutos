b820bf50bd4e870a044ffcc26f1d731763db4ee3
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-02-24 18:40:24 -0300

    consumindo api com o axios

--- frontprodutos/src/Components/Produtos/Produtos.jsx
+++ frontprodutos/src/Components/Produtos/Produtos.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Produtos = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://localhost:7193/Produtos")
+      .then((response) => {
+        console.log(response.data);
+        setProdutos(response.data);
+      })
+      .catch(() => {
+        console.log("Deu errado");
+      });
+  }, []);
+
   return (
     <div>
-      <h1>teste</h1>
+      {produtos.map((produto, key) => {
+        return (
+          <div className="card" key={key}>
+            <div className="card-body">
+              <h1>{produto.nome}</h1>
+              <h1>{produto.preco}</h1>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
